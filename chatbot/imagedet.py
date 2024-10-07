b0ef83ba497b06900148c7449911242b174eef23
amujkic1 <amujkic1@etf.unsa.ba>
2024-10-07 16:43:42 +0200

    model loaded

---
+++ chatbot/imagedet.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from inference_sdk import InferenceHTTPClient
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key=api_key
+)
+
+result = CLIENT.infer(r"C:\Users\HP\Desktop\apples.jpg", model_id="yolo-waste-detection/1")
+print(result)
