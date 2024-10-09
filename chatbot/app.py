5621fadf92ce7de5b7ccc6df3c0d8a6a668b4e73
amujkic1 <amujkic1@etf.unsa.ba>
2024-10-09 16:47:37 +0200

    connected frontend to the api

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from imagedet import CLIENT
+import os
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
+
+# Create a directory to save uploaded images
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
 def hello_world():
@@ -11,10 +16,23 @@ def hello_world():
 
 @app.route('/api/sort-garbage', methods=['POST'])
 def sort_garbage():
+
+    if 'image' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+    
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    
+    # Save the file
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
     print("sorting")
     # instead of hardcoding the image use the one from html formData
-    result = CLIENT.infer(r"C:\Users\HP\Desktop\DropCycle_img\apples.jpg", model_id="yolo-waste-detection/1")
+    result = CLIENT.infer(file_path, model_id="yolo-waste-detection/1")
     detected_class = result['predictions'][0]['class']
+    
     return jsonify({'type': detected_class})
 
 if __name__ == '__main__':
