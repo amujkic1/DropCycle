9ad21d09a11fc4a8e6c21a451510950cbf508131
amujkic1 <amujkic1@etf.unsa.ba>
2024-10-09 15:17:10 +0200

    implemented function for waste sorting using external API

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from imagedet import CLIENT
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -8,5 +9,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/api/sort-garbage', methods=['POST'])
+def sort_garbage():
+    print("sorting")
+    # instead of hardcoding the image use the one from html formData
+    result = CLIENT.infer(r"C:\Users\HP\Desktop\DropCycle_img\apples.jpg", model_id="yolo-waste-detection/1")
+    detected_class = result['predictions'][0]['class']
+    return jsonify({'type': detected_class})
+
 if __name__ == '__main__':
     app.run()
+    
